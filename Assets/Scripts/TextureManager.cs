08c1b4cdfde16409df1460e776455efa68c2a549
Torantulino <40276179@live.napier.ac.uk>
2019-10-18 15:52:55 +0100

    Modifies TextureManager to send 3D texture coordinates to shader.

--- Assets/Scripts/TextureManager.cs
+++ Assets/Scripts/TextureManager.cs
@@ -27,57 +27,39 @@ public class TextureManager
             if (block_material == null)
             {
                 block_material = Resources.Load<Material>("Materials/BlockMaterial");
-                block_material.SetTexture("_MyArr", Texture_Array);
+                block_material.SetTexture("_TexArr", Texture_Array);
             }
             return block_material;
         }
     }
-
-
 
     // Creates a TextureArray (Only works on modern GPUs) which appears 
     // to the GPU as a single object and uses a single drawcall
     public Texture2DArray CreateTextureArray()
     {
         // //Load textures
-        // block_textures = Resources.LoadAll<Texture2D>("Textures/Blocks/Opaque");
-
-        // // Create texture array
-        // Texture2DArray _texture_array = new Texture2DArray(
-        //     TEXTURE_SIZE_PX,
-        //     TEXTURE_SIZE_PX,
-        //     block_textures.Length,
-        //     TextureFormat.RGBA32,
-        //     true,
-        //     false);
-
-        // // Settings
-        // _texture_array.wrapMode = TextureWrapMode.Repeat;
-        // _texture_array.filterMode = FilterMode.Bilinear;
-
-        // // Iterate over textures adding pixel data to Texture Array
-        // for (int i = 0; i < block_textures.Length; i++)
-        // {
-        //     _texture_array.SetPixels(block_textures[i].GetPixels(0), i, 0);
-        // }
-
-        // // Apply changes
-        // _texture_array.Apply();
-
         Texture2D[] block_textures = Resources.LoadAll<Texture2D>("Textures/Blocks/Opaque");
-        // CHANGEME: TextureFormat.RGB24 is good for PNG files with no alpha channels. Use TextureFormat.RGB32 with alpha.
-        // See Texture2DArray in unity scripting API.
-        Texture2DArray _texture_array = new Texture2DArray(16, 16, block_textures.Length, TextureFormat.RGB24, false);
 
-        // CHANGEME: If your files start at 001, use i = 1. Otherwise change to what you got.
+        // Create texture array
+        Texture2DArray _texture_array = new Texture2DArray(
+            TEXTURE_SIZE_PX,
+            TEXTURE_SIZE_PX,
+            block_textures.Length,
+            TextureFormat.RGB24,
+            false);
+
+        // Settings
+        _texture_array.wrapMode = TextureWrapMode.Repeat;
+        _texture_array.filterMode = FilterMode.Bilinear;
+
+        // Iterate over textures adding pixel data to Texture Array
         for (int i = 0; i < block_textures.Length; i++)
         {
-            Debug.Log("Loading " + block_textures[i].name);
-            Texture2D tex = block_textures[i];
-            _texture_array.SetPixels(tex.GetPixels(0), i, 0);
+            //Debug.Log("Loading " + block_textures[i].name);
+            Texture2D _texture = block_textures[i];
+            _texture_array.SetPixels(_texture.GetPixels(0), i, 0);
         }
         _texture_array.Apply();
-
 
         return _texture_array;
     }
