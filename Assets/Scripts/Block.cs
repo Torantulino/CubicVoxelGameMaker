cfa42eb25842802c52b13a8b4dc39eef92180ddd
Torantulino <40276179@live.napier.ac.uk>
2019-10-19 17:49:17 +0100

    Improves face creation algorithm and adds faces to collection.

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -9,55 +9,39 @@ public class Block
     public bool hitboxEnabled;
 
     public GameObject Game_Object;
+    public BlockFace[] Faces = new BlockFace[6];
+    private Vector3[] normals = new Vector3[6]
+    {
+        Vector3.left,
+        Vector3.right,
+        Vector3.forward,
+        Vector3.back,
+        Vector3.up,
+        Vector3.down
+    };
 
-    public Block(int _type, Vector3 _pos, bool _hitboxEnabled = true){
+    public Block(int _type, Vector3 _pos, bool _hitboxEnabled = true)
+    {
+        // Set properties
         type = _type;
         hitboxEnabled = _hitboxEnabled;
         
+        // Create GameObject
         GameObject Game_Object = new GameObject("Block");
 
-        BlockFace b3 = new BlockFace(
-            texture_manager.Block_Material,
-            BlockInfo.Face_Textures[_type, 0],
-            Vector3.left
-        );
-        b3.Game_Object.transform.parent = Game_Object.transform;
-
-        BlockFace b4 = new BlockFace(
-            texture_manager.Block_Material,
-            BlockInfo.Face_Textures[_type, 1],
-            Vector3.right
-        );
-        b4.Game_Object.transform.parent = Game_Object.transform;
-
-        BlockFace b = new BlockFace(
-            texture_manager.Block_Material,
-            BlockInfo.Face_Textures[_type, 2],
-            Vector3.forward
-        );  
-        b.Game_Object.transform.parent = Game_Object.transform;
-
-        BlockFace b2 = new BlockFace(
-            texture_manager.Block_Material,
-            BlockInfo.Face_Textures[_type,3],
-            Vector3.back
-        ); 
-        b2.Game_Object.transform.parent = Game_Object.transform;
-
-        BlockFace b5 = new BlockFace(
-            texture_manager.Block_Material,
-            BlockInfo.Face_Textures[_type, 4],
-            Vector3.up
-        );
-        b5.Game_Object.transform.parent = Game_Object.transform;
-
-        BlockFace b6 = new BlockFace(
-            texture_manager.Block_Material,
-            BlockInfo.Face_Textures[_type, 5],
-            Vector3.down
-        );
-        b6.Game_Object.transform.parent = Game_Object.transform;
-
+        // Create faces
+        for (int i=0; i<6; i++)
+        {
+            BlockFace b = new BlockFace(
+                texture_manager.Block_Material,
+                BlockInfo.Face_Textures[_type, i],
+                normals[i]
+            );
+            b.Game_Object.transform.parent = Game_Object.transform;
+            b.Game_Object.name = "Face_" + i;
+            Faces[i] = b;
+        }
+        // Set object positions
         Game_Object.transform.position = _pos;
     }
 }
