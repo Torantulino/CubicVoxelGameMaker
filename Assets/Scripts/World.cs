831f111cfc2cd5c7447f2b9654840a6b8c6dff6a
Torantulino <40276179@live.napier.ac.uk>
2019-10-22 04:39:33 +0100

    Implements inner chunk face culling. Ensuring unseen faces aren't rendered.

--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -31,9 +31,58 @@ public class World
         yield return new WaitForSeconds(0);
     }
 
+    public void UpdateWorld()
     {
-        Chunk _chunk = new Chunk();
+        foreach (KeyValuePair<Vector2Int, Chunk> _pair in Chunks)
+        {
+            Chunk _chunk = _pair.Value;
+            CullHiddenFaces(_chunk);
+        }
+    }
 
-        Chunks[_chunk_pos.Item1,_chunk_pos.Item2, _chunk_pos.Item3] = _chunk; 
+    private void CullHiddenFaces(Chunk _chunk)
+    {
+        if(!_chunk.hasLoaded)
+            return;
+
+        for (int x = 0; x < CHUNK_SIZE; x++)
+        {
+            for (int y = 0; y < WORLD_HEIGHT; y++)
+            {
+                for (int z = 0; z < CHUNK_SIZE; z++)
+                {
+                    try
+                    {
+                        // X
+                        if (x + 1 != CHUNK_SIZE && _chunk.blocks[x + 1, y, z].Game_Object != null)
+                            _chunk.blocks[x + 1, y, z].Faces[1].Game_Object.SetActive(false);
+                        // -X 
+                        if (x != 0 && _chunk.blocks[x - 1, y, z].Game_Object != null)
+                            _chunk.blocks[x - 1, y, z].Faces[0].Game_Object.SetActive(false);
+                        // Z
+                        if (z + 1 != CHUNK_SIZE && _chunk.blocks[x, y, z + 1].Game_Object != null)
+                            _chunk.blocks[x, y, z + 1].Faces[3].Game_Object.SetActive(false);
+                        // -Z 
+                        if (z != 0 && _chunk.blocks[x, y, z - 1].Game_Object != null)
+                            _chunk.blocks[x, y, z - 1].Faces[2].Game_Object.SetActive(false);
+                        // Y
+                        if (y  + 1 != WORLD_HEIGHT && _chunk.blocks[x, y + 1, z].Game_Object != null)
+                            _chunk.blocks[x, y + 1, z].Faces[5].Game_Object.SetActive(false);
+                        // -Y 
+                        if (y != 0 && _chunk.blocks[x, y - 1, z].Game_Object != null)
+                            _chunk.blocks[x, y - 1, z].Faces[4].Game_Object.SetActive(false);
+                    }
+                    catch (System.IndexOutOfRangeException)
+                    {
+                        Debug.LogError("Out of Range at: (" + x +", " + y + ", " + z + ")");
+                        throw;
+                    }
+
+                    if(x==0)
+                    _chunk.blocks[x + 1, y, z].Faces[1].Game_Object.transform.localScale = Vector3.one * 10.0f;
+
+                }
+            }
+        }
     }
 }
