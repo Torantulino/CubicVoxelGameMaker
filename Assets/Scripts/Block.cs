72db5b5584653cdc9451ce7a9e227fa3b79acd4d
Torantulino <40276179@live.napier.ac.uk>
2019-12-18 12:50:02 +0000

    Removes unused variable from Block.

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -6,7 +6,6 @@ using UnityEngine;
 public class Block
 {
     public int type = (int)BlockInfo.BlockType.Air;
-    public bool hitboxEnabled;  //TODO: Use inheritance to remove unncessary variables from standard blocks?
     public BlockFace[] Faces = new BlockFace[6];
     public Vector3 Position;
 
@@ -15,14 +14,10 @@ public class Block
     {
         // Set properties
         type = _type;
-        hitboxEnabled = _hitboxEnabled;
         Position = _pos;
 
         if (_type == (int)BlockInfo.BlockType.Air)
-        {
-            hitboxEnabled = false;
             return;
-        }
 
         // Create faces
         for (int i = 0; i < 6; i++)
